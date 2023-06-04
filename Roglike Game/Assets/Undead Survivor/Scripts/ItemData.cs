37b7c97c23e4cfbacf85ce6c292eb9179787c49a
WooJaeHak <wistm6980@gmail.com>
2023-06-04 18:46:32 +0900

    Scriptable Object

---
+++ Roglike Game/Assets/Undead Survivor/Scripts/ItemData.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//커스템 메뉴를 생성하는 속성
+[CreateAssetMenu(fileName = "Item", menuName = "Scriptable Object/ItemData")]
+public class ItemData : ScriptableObject
+{
+    public enum ItemType { Melee, Range, Glove, Shoe, Heal }
+
+    [Header("# Main Info")]
+    public ItemType itemType;
+    public int itemId;
+    public string itemName;
+    public string itemDesc;
+    public Sprite itemIcon;
+
+    [Header("# Level Data")]
+    public float baseDamage;
+    public int baseCount;//관통
+
+    //레벨업
+    public float[] damages;
+    public int[] count;
+
+    [Header("# Weapon")]
+    public GameObject projectile;
+}
